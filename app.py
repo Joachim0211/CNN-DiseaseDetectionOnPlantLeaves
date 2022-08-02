4074c4142a5847941d91f74ce96a6764f4155c8e
Joachim <joachim0509@gmail.com>
2022-08-02 17:44:53 +0200

    Added required files for heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from unicodedata import name
 from flask import Flask, jsonify, request, render_template
-from flask_dropzone import Dropzone
+# from flask_dropzone import Dropzone
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,26 +8,26 @@ print(basedir)
 
 app = Flask(__name__)
 
-app.config.update(
-    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
-    #Flask-Dropzone config:
-    DROPZONE_MAX_FILE_SIZE=1024,  # set max size limit to a large number, here is 1024 B
-    DROPZONE_TIMEOUT=5 * 60 * 1000  # set upload timeout to a large number, here is 5 minutes
-)
+# app.config.update(
+#     UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+#     #Flask-Dropzone config:
+#     DROPZONE_MAX_FILE_SIZE=1024,  # set max size limit to a large number, here is 1024 B
+#     DROPZONE_TIMEOUT=5 * 60 * 1000  # set upload timeout to a large number, here is 5 minutes
+# )
 
-dropzone = Dropzone(app)
+# dropzone = Dropzone(app)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def upload():
-    if request.method == 'POST':
-        f = request.files.get('file')
-        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    # if request.method == 'POST':
+    #     f = request.files.get('file')
+    #     f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
     return "Hello World" #render_template('index.html')
 
-@app.route('/answer')
-def up():
-    return "Your picture is a tiger"  #render_template('index.html')
+# @app.route('/answer')
+# def up():
+#     return "Your picture is a tiger"  #render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
