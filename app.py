d0078338e7d0e78317cd612e3170f651ae4afdf2
Joachim <joachim0509@gmail.com>
2022-08-02 21:35:22 +0200

    Added required files for heroku

--- app.py
+++ app.py
@@ -30,6 +30,5 @@ def upload():
 #     return "Your picture is a tiger"  #render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-app.run(port = 5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
