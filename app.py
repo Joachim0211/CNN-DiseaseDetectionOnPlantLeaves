daf9f085ac78f9303acad1c32b4bb948040066c7
Joachim <joachim0509@gmail.com>
2022-08-04 09:55:25 +0200

    new ui functioning dropzone

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from crypt import methods
 from unicodedata import name
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, url_for, Response
 from flask_dropzone import Dropzone
+import numpy as np
+from PIL import Image
+import PIL
+from io import BytesIO
+import tensorflow as tf
 import os
+import time
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 print(basedir)
+predictions = []
+
 
 app = Flask(__name__)
 
@@ -17,14 +24,52 @@ app.config.update(
 )
 
 dropzone = Dropzone(app)
+MODEL = tf.keras.models.load_model("./models/3")
+class_names = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+global submission
+submission = 'Your file name'
+#global result
 
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        f = request.files.get('file')
-        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    f = request.files.get('file')
+    global result1
+    if 'Early' in f.filename:
+        result1 = 'Early Blight'
+    elif 'Late' in f.filename:
+        result1 = 'Late Blight'
+    elif 'RS_HL' in f.filename:
+        result1 = 'Your picture was labeled "Healthy"'
+    else:
+        result1 = 'Your picture was not clearly labeled'
+    #f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    img = np.array(PIL.Image.open(f))
+    img_batch = np.expand_dims(img, 0)      
+    predictions = MODEL.predict(img_batch)
+    score = tf.nn.softmax(predictions[0])
+    global result
+    if 100 - np.max(score) < 60:
+        result = "The analysis shows no clear result"
+    else:
+        if class_names[np.argmax(score)] == 'Potato___healthy':
+            result = "Your plant is healthy with a {:.2f} percent confidence.".format(100 - np.max(score))
+        elif class_names[np.argmax(score)] == 'Potato___Early_blight':
+            result = "Your plant shows symptoms of {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 - np.max(score))
+        elif class_names[np.argmax(score)] == 'Potato___Late_blight':
+            result = "Your plant shows symptoms of {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 - np.max(score))
+    
+    time.sleep(3)
+    print('woken up')
+
+
+    return jsonify(data=result, errors=result1)
+
+
+
+@app.route('/')
+def show():    
     return render_template('index.html')
+
 
 @app.route('/answer')
 def up():
@@ -32,4 +77,4 @@ def up():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
