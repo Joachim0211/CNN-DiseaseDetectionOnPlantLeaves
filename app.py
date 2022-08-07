315c32fd29352e839bdd83645733dad2e5602b49
Joachim <joachim0509@gmail.com>
2022-08-07 19:26:18 +0200

    resize picture working

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import numpy as np
 from PIL import Image
 import PIL
 from io import BytesIO
+import io
 import tensorflow as tf
 import os
+import cv2 as cv
+import base64
 import time
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -32,22 +35,50 @@ submission = 'Your file name'
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    f = request.files.get('file')
-    global result1
-    if 'Early' in f.filename:
-        result1 = 'Early Blight'
-    elif 'Late' in f.filename:
-        result1 = 'Late Blight'
-    elif 'RS_HL' in f.filename:
-        result1 = 'Your picture was labeled "Healthy"'
-    else:
-        result1 = 'Your picture was not clearly labeled'
+    a = request.files.get('file')
+    f = request.files.get('file').read()
+    npimg = np.fromstring(f, np.uint8)
+    
     #f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
-    img = np.array(PIL.Image.open(f))
-    img_batch = np.expand_dims(img, 0)      
+    print(type(npimg))
+    #ff = Image.open(f'./uploads/{f.filename}')
+    #img_0 = PIL.Image.open(ff)
+    
+    img = cv.imdecode(npimg,cv.IMREAD_COLOR)
+    img_0 = cv.resize(img,(256,256))
+    print(img)
+    #img_0 = img.resize((256,256),  Image.Resampling.LANCZOS)
+    img_1 = np.array(img_0)
+    print(img_1.shape)
+    img_batch = np.expand_dims(img_1, 0)
+    print(img_batch.shape) 
+    #img_array = tf.keras.utils.img_to_array(img)
+
+    # img_batch = tf.expand_dims(img_array, 0)
+
     predictions = MODEL.predict(img_batch)
     score = tf.nn.softmax(predictions[0])
-    global result
+    
+   
+    # global result1
+    if 'Early' in a.filename:
+        result1 = 'Early Blight'
+    elif 'RS_LB' in a.filename:
+        result1 = 'Late Blight'
+    elif 'RS_HL' in a.filename:
+        result1 = 'Your picture was labeled "Healthy"'
+    else:
+        result1 = a.filename
+    #f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    
+    # img = tf.image.resize(f, [256, 256])
+    # # img_array = tf.keras.utils.img_to_array(img)
+    # # img_batch = tf.expand_dims(img_array, 0)
+    # # img.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    # img_1 = np.array(PIL.Image.open(img))
+    # img_batch = np.expand_dims(img_1, 0)      
+    #img = tf.image.resize(f, [256, 256])
+    
     if 100 - np.max(score) < 60:
         result = "The analysis shows no clear result"
     else:
@@ -58,7 +89,7 @@ def upload():
         elif class_names[np.argmax(score)] == 'Potato___Late_blight':
             result = "Your plant shows symptoms of {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 - np.max(score))
     
-    time.sleep(3)
+    # time.sleep(3)
     print('woken up')
 
 
